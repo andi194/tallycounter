d69e7a2069a1b617a62d8e4663e3a3a8f4743ace
andi194 <58745968+andi194@users.noreply.github.com>
2020-09-29 15:40:57 +0100

    react calculator v1
    calculator v1

--- src/App.js
+++ src/App.js
@@ -1,75 +1,75 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import './App.css';
-import Counter from './counter.js';
+import React, { Component } from 'react';
+import './App.scss';
+import ResultComponent from './ResultComponent';
+import KeyPadComponent from "./KeyPadComponent";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      counterRefs: [],
-      counters: []
+class App extends Component {
+    constructor(){
+        super();
+
+        this.state = {
+            result: ""
+        }
     }
-  }
 
-  resetHandler() {
-    this.state.counterRefs.forEach((r) => r.current.resetCounter())
-  }
+    onClick = button => {
 
+        if(button === "="){
+            this.calculate()
+        }
 
-  createCounter(i, ref) {
-    return (
-      <Counter counterName={"counter" + (i + 1)} key={i} ref={ref} />
-    )
-  }
+        else if(button === "C"){
+            this.reset()
+        }
+        else if(button === "CE"){
+            this.backspace()
+        }
 
-  addCounter() {
-    this.setState((state) => {
-      const ref = React.createRef();
-      const newCounter = this.createCounter(state.counters.length, ref);
-      return ({
-        counters: state.counters.concat(newCounter),
-        counterRefs: state.counterRefs.concat(ref)
-      })
-    })
-  }
-
-  removeCounter() {
-    this.setState((state) => {
-      return ({
-        counters: state.counters.slice(0, state.counters.length - 1),
-        counterRefs: state.counterRefs.slice(0, state.counterRefs.length - 1)
-      })
-    })
-  }
+        else {
+            this.setState({
+                result: this.state.result + button
+            })
+        }
+    };
 
 
-  render() {
-    return (
-      <>
-        <Container>
-          <Row>
-            <Col className="text-center">
-              <Button onClick={() => this.resetHandler()} > Reset All</Button>
-            </Col>
-            <Col className="text-center">
-              <Button onClick={() => this.addCounter()} > Add Counter</Button>
-            </Col>
-            <Col className="text-center">
-              <Button onClick={() => this.removeCounter()} > Remove Counter</Button>
-            </Col>
-          </Row>
-          <Row>
-            {this.state.counters}
-          </Row>
-        </Container>
-      </>
-    );
-  }
+    calculate = () => {
+        try {
+            this.setState({
+                
+                result: (eval(this.state.result) || "" ) + ""
+            })
+        } catch (e) {
+            this.setState({
+                result: "error"
+            })
+
+        }
+    };
+
+    reset = () => {
+        this.setState({
+            result: ""
+        })
+    };
+
+    backspace = () => {
+        this.setState({
+            result: this.state.result.slice(0, -1)
+        })
+    };
+
+    render() {
+        return (
+            <div>
+                <div className="calculator-body">
+                    <h1>Simple Calculator</h1>
+                    <ResultComponent result={this.state.result}/>
+                    <KeyPadComponent onClick={this.onClick}/>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
